45827d169a43422ab7f7b1722dfaa4e4e4be4a14
Jeremiah Mateo <jeremiah.mateo4321@gmail.com>
2026-03-18 14:35:27 +0400

    Start to format preview page

--- src/components/preview/Preview.jsx
+++ src/components/preview/Preview.jsx
@@ -1,5 +1,7 @@
 import * as configModule from "../../data/data-configs";
 
+import "../../styles/preview.css"
+
 export default function Preivew({ setIsSubmitted, person }) {
   const configs = Object.values(configModule);
   
@@ -7,31 +9,47 @@ export default function Preivew({ setIsSubmitted, person }) {
     setIsSubmitted(false);
   }
     
-  let index = 0;
+  const stringEntries = Object.entries(person)
+                          .filter(([key, value]) => typeof value === "string" && !key.includes("name"));
   return (
     <>
-      <p>{person.fname}</p>
-      <p>{person.lname}</p>
-      <p>{person.phone}</p>
-      <p>{person.email}</p>
-      
-      {
-        configs.map(obj => {
-          const data = person[obj.name];
-
-          if (Array.isArray(data)) {
-            return data.map((item, i) =>
-              obj.fields.map(field => (
-                <p key={`${obj.name}-${i}-${field.attribute}`}>
-                  {item?.[field.attribute]}
-                </p>
+      <div className="preview">
+        <div className="general-info">
+          <p className="name">{person.fname} {person.lname}</p>
+          
+          {
+            stringEntries
+              .map(([key, value]) => (
+                  <p 
+                    key={key}
+                    className={key}
+                  >
+                    {value}
+                  </p>
               ))
-            );
-          }
+          }  
+        </div>
+        
+        <div className="experience">
+          {
+            configs.map(obj => {
+              const data = person[obj.name];
 
-          return null;
-        })
-      }
+              if (Array.isArray(data)) {
+                return data.map((item, i) =>
+                  obj.fields.map(field => (
+                    <p key={`${obj.name}-${i}-${field.attribute}`}>
+                      {item?.[field.attribute]}
+                    </p>
+                  ))
+                );
+              }
+
+              return null;
+            })
+          }
+        </div>
+      </div>
       
       <button type="button" onClick={handleClick}>Edit</button>
     </>
